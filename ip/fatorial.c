4e6b7dd83b60bb2dfe755f63fb7eaa9a99b660f0
Emanuel Almeida <cemanuel59344@icloud.com>
2026-09-09 01:43:00 -0300

    adiciona programa de cálculo de fatorial em C

---
+++ ip/fatorial.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+    int n, fatorial = 1;
+
+    printf("Digite um número: ");
+    scanf("%d", &n);
+
+    do {
+        if (n < 0) {
+            printf("Digite um número inteiro positivo.\n");
+        }
+        printf("Digite um número: ");
+        scanf("%d", &n);
+    } while (n < 0);
+
+    for (int i = 1; i <= n; i++) {
+        fatorial = fatorial*i;
+    }
+
+    printf("O fatorial de %d é: %d\n", n, fatorial);
+
+    return 0;
+}
