b6565338ff20793258d28bb76e53591a716c90ed
Emanuel Almeida <cemanuel59344@icloud.com>
2026-09-08 21:37:59 -0300

    Initial Commit

---
+++ ip/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ip
+//
+//  Created by Emanuel on 08/09/26.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
